f7b1d893169c9e9a423b9e8569604c98ac15270d
jprei <jprei@wisc.edu>
2026-06-05 14:48:17 -0500

    Player can attack
    Spawns a hitbox in front of him to attack.

--- Entities/player.gd
+++ Entities/player.gd
@@ -4,6 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+#Siblings
+@onready var sprite := $Sprite
+@onready var attack_pivot := $AttackPivot
+@onready var sword_hitbox := $AttackPivot/SwordHitBox/CollisionShape2D
+
+#Attack Variables
+@export var  attack_duration: float = 0.15
+
+
+func _ready() -> void:
+	attack_pivot.visible = false
+	sword_hitbox.disabled = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +23,32 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("A_Button") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("D_Pad_Left", "D_Pad_Right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			sprite.flip_h = true
+		elif direction < 0:
+			sprite.flip_h = false
+		attack_pivot.scale.x = direction * -1
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if Input.is_action_just_pressed("B_Button") and sword_hitbox.disabled:
+		_trigger_attack()
 
 	move_and_slide()
+	
+
+func _trigger_attack() -> void:
+	attack_pivot.visible = true
+	sword_hitbox.disabled = false
+	
+	await get_tree().create_timer(attack_duration).timeout
+	
+	attack_pivot.visible = false
+	sword_hitbox.disabled = true
