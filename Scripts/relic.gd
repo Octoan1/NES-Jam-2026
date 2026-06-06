fdaaca9ae67c1831b0c00df3ada50a0e52da415e
Connradical <conneremoore@gmail.com>
2026-06-05 22:09:13 -0500

    Relics added
    - 12 relics added as special Relic resources.
    - Each relic is currently missing their implemented effect, as well as their sprite. But the framework is there.

--- Scripts/relic.gd
+++ Scripts/relic.gd
@@ -1,11 +1,10 @@
-class_name relic
 extends Resource
+class_name Relic
 
-var relic_name: String
-var pro: String
-var con: String
 
-func _init(r_name: String = "Undefined", r_pro: String = "Undefined", r_con: String = "Undefined"):
-	relic_name = r_name
-	pro = r_pro
-	con = r_con
+@export var name: String
+@export var description: String
+@export var icon: Texture2D
+@export var pro: String
+@export var con: String
+@export var effect_name: String
